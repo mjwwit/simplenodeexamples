16ec76aa2e9bd57a22184c3d29fc475fc7559551
Anurag Shrivastava <anurag@kennissoftware.com>
2015-08-13 08:50:24 +0200

    Added prototype and trace examples

---
+++ trace.js
@@ -0,0 +1,8 @@
+console.time('myapp');
+var d = new Date();
+var today = d.getDate();
+console.trace();
+console.log(today);
+console.log(d.getMonth());
+console.log(d.getFullYear());
+console.timeEnd('myapp');
